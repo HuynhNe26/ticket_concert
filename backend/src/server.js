957ba3ba211ca911d3b01c3506c21b1217386bb4
Minkvux <Phungminhvu17102005@gmail.com>
2026-01-11 14:20:56 +0700

    Update

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ import { connectDB } from "./config/database.js";
 
 dotenv.config();
 
-import { initZoneSocket } from "./socket/users/zone.js";
+import { initZoneSocket } from "./socket/zone.js";
 
 // users
 import authRouter from "./router/users/user.js";
